fc5a1957e872aed96fb2169c9b8b017ac52ad264
Daniel Castro <danielsobretudo@yahoo.com.br>
2021-08-26 13:31:02 -0300

    add controller and templates

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,13 +1,43 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
+
   def index
     @cocktails = Cocktail.all
   end
 
   def show
-    @cocktail = Cocktail.find(params[:id])
   end
 
   def edit
+  end
+
+  def update
+    @cocktail.update(cocktail_params)
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+    redirect_to cocktails_path
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path(@cocktail)
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :ingredients, :stars, :image_url, :recipe)
+  end
+
+  def set_cocktail
     @cocktail = Cocktail.find(params[:id])
   end
 end
