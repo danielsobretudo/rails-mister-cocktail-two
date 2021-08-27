d98306d5314600557a30e9eeb9d525050515344c
Daniel Castro <danielsobretudo@yahoo.com.br>
2021-08-26 21:16:49 -0300

    new seeds

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,5 @@
 class Cocktail < ApplicationRecord
   validates :name, presence: true
   validates :ingredients, :recipe, presence: true
-  validates :stars, inclusion: { in: (1..5) }, numericality: { message: "%{value} seems wrong" }
+  validates :stars, inclusion: { in: (1..5) }
 end
